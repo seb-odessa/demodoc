a4628f82db0b67ef98a7c31c2d27eb59448c6025
seb-odessa <seb@ukr.net>
2016-03-10 12:04:33 +0200

    Init

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! A demo library.
+//! This is a crate level description
+
+
+/// Unusual Foo struct with the some payload
+pub struct Foo{
+    /// The payload contains payload of the structure
+    payload : String
+}
+
+#[test]
+fn it_works() {}
