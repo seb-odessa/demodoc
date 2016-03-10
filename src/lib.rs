b76795c64512463a8186ddc372ba65dfc5af840f
seb-odessa <seb@ukr.net>
2016-03-10 12:26:26 +0200

    Added script for deploy docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
-//! A demo library.
-//! This is a crate level description
+//! Demo library. This is a crate level description
 
-
-/// Unusual Foo struct with the some payload
+/// Foo struct with the some payload
 pub struct Foo{
     /// The payload contains payload of the structure
     payload : String
 }
 
+pub fn get_true() -> bool {
+    //! This function will return true
+    return true;
+}
+
 #[test]
-fn it_works() {}
+fn it_works() 
+{
+//! Recalibrate a pesky universe (this is also a doc comment, like above,
+}
